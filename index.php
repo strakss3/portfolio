6baeaa480d6d18d52d3aa7d5760e6e12d55eefba
Ethan <ethan.layan@gmail.com>
2026-06-18 20:18:48 +0200

    -

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+    <title>Welcome to Ethan's PortFolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="icon.ico">
+    <link rel="stylesheet" href="style.css">
+
+    <script>
+
+    </script>
+</HEAD>
+<BODY>
+    
+</BODY>
+</HTML>
